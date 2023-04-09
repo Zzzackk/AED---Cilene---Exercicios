f85d50d06e386877474b0a5fa3484375131f1b33
Isaac Vono <110258968+Zzzackk@users.noreply.github.com>
2023-04-09 17:53:05 -0300

    initial commit

---
+++ aed-2an-isaac-ex1.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Isaac Vono Rodrigues | 2AN | 30/03/2023
+
+AED2 | Lista de Exercicios | Exercicio 1
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int dif(int n1, int n2)
+{
+    return (n1-n2);
+}
+
+
+void main()
+{
+    int n1, n2;
+    printf("Digite dois numeros: \n");
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    printf("A diferenca entre os dois numeros e %d",dif(n1,n2));
+    
+}
